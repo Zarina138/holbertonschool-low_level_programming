88348fc7aba1263c96a239b90879282f2ea2b6af
Zarina138 <zrinaabdulazizova@gmail.com>
2025-07-22 03:58:53 +0000

    Task-Puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints a specific message using puts function
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     puts("Programming is like building a multilingual puzzle");
