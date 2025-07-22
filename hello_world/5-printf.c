fa3a8162c043c12e51005658c9df3395118a785c
Zarina138 <zrinaabdulazizova@gmail.com>
2025-07-22 04:18:14 +0000

    Task-Printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description - Prints a spesific message using printf function
+ *
+ * Return - Always 0 (Success)
+
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
+    printf("with proper grammar, but the outcome is a piece of art,\n");
     return 0;
 }
-
