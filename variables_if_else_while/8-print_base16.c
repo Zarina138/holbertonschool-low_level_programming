290877ec9c0d8d901d48ecec3bccfae0317005eb
Zarina138 <zrinaabdulazizova@gmail.com>
2025-07-24 14:09:05 +0000

    Task8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {
+   char C;
+   
+
+   for(C = '0' ;C <='9' ;C++)
+   {
+      putchar(C);
+   }
+   for (C = 'a';C <='f';C++ )
+   {
+   		putchar(C);
+   	
+   }
+   putchar('\n');
+   
+   return 0;
+}
