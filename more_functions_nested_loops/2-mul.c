197dee7fa934c69e0b6820954624f68d638ba762
Zarina138 <zrinaabdulazizova@gmail.com>
2025-07-28 15:19:47 +0000

    Task2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *
+ * @a: The character
+ * @b: The character
+ *
+ * Return: 1 if c is digit, 0 otherwise
+ */
+
+int mul(int a, int b);
+{
+    _putchar(a*b);
+    return (0);
+}
