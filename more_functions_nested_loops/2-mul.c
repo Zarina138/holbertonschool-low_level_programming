af07658034fce1c12ffc4bb7a4d60a8c4fbe0cad
Zarina138 <zrinaabdulazizova@gmail.com>
2025-07-28 15:24:32 +0000

    Task2

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -5,11 +5,11 @@
  * @a: The character
  * @b: The character
  *
- * Return: 1 if c is digit, 0 otherwise
+ * Return: jasjf
  */
 
 int mul(int a, int b)
 {
-    int mul(int a, int b);
-    return (a*b);
+	int mul(int a, int b);
+	return (a * b);
 }
