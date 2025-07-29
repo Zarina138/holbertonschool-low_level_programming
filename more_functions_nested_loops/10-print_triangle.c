b2b6a59fa188e3c74908f35f490b60438c995336
Zarina138 <zrinaabdulazizova@gmail.com>
2025-07-29 16:42:55 +0000

    Task10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -18,7 +18,7 @@ void print_triangle(int size)
 
 	for (i = 1; i <= size; i++)
 	{
-		for (j = 1; j < size - i; j++)
+		for (j = 1; j <= size - i; j++)
 			_putchar(' ');
 		for (j=1; j<=i; j++)
 			_putchar('#');
