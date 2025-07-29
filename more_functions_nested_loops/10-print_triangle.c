5aefa2f7dd2078541e68322b9d861ec30019f58d
Zarina138 <zrinaabdulazizova@gmail.com>
2025-07-29 16:36:16 +0000

    Task10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle hehehe
+ * @n: nanana
+ *
+ * Description: Bla bla bla
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('#');
+		_putchar('\n');
+	}
+}
