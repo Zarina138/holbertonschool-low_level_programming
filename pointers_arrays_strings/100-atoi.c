5b70b290bfdca7be5ffc3603c5a5fa3e38c5c1c2
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-01 18:40:25 +0000

    Task

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,26 +8,36 @@
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int isare = 1;
-	int netice = 0;
-	int regem = 0;
+    int i = 0;
+    int sign = 1;
+    int result = 0;
+    int digit_found = 0;
 
-	while (s[i])
-	{
-		if (s[i] == '-')
-			isare *= -1;
-		else if (s[i] == '+')
-		    isare *= 1;
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			regem = 1;
-			netice = netice * 10 + (s[i] - '0');
-		}
-		else if (regem)
-			break;
-		i++;
-	}
+    while (s[i])
+    {
+        if (s[i] == '-')
+            sign *= -1;
+        else if (s[i] == '+')
+            sign *= 1;
+        else if (s[i] >= '0' && s[i] <= '9')
+        {
+            int digit = s[i] - '0';
 
-	return (isare * netice);
+            if (result > (2147483647 - digit) / 10)
+            {
+                if (sign == 1)
+                    return (2147483647);
+                else
+                    return (-2147483648);
+            }
+
+            digit_found = 1;
+            result = result * 10 + digit;
+        }
+        else if (digit_found)
+            break;
+        i++;
+    }
+
+    return (sign * result);
 }
