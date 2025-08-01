4a205f3b0ac2b642610808d5cff976cb95239c99
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-01 18:00:15 +0000

    Task

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,21 +1,28 @@
 #include "main.h"
 
+/**
+ * rev_string - Reverses a string
+ * @s: The string to be reversed
+ *
+ * Description: This function reverses the characters in a string
+ * using a two-pointer approach.
+ */
 void rev_string(char *s)
 {
-    int i = 0, j = 0;
-    char z;
+	int i = 0, j = 0;
+	char z;
 
-    while (s[j] != '\0')
-        j++;
+	while (s[j])
+		j++;
 
-    j--;
+	j--;
 
-    while (i < j)
-    {
-	    z = s[i];
-	    s[i] = s[j];
-	    s[j] = z;
-	    i++;
-	    j--;
-    }
+	while (i < j)
+	{
+		z = s[i];
+		s[i] = s[j];
+		s[j] = z;
+		i++;
+		j--;
+	}
 }
