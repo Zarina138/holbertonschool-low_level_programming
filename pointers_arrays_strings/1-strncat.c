62be064463b11436c3415091ae589c77705782f9
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-02 09:18:00 +0000

    Task

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - stringlerimiizi birlesdirirr
+ * @dest: destttinationn hehhe
+ * @src: sourceeeee
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+    int i = 0;
+    int j = 0;
+
+    while (dest[i])
+    {
+        i++;
+    }
+
+    while (j < n && src[j] != '\0')
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+
+    dest[i] = '\0';
+
+    return dest;
+}
