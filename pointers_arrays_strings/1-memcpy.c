eee3e1b593f4d7152c6fd6be95c5dd1210349737
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-04 21:32:24 +0000

    Task

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * memcpy - yaddas sahesini copy ediyir
+ * @dest: destination
+ * @src: source
+ * @n: baytlarimizin sayi hehe
+ *
+ * Return: returun dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
