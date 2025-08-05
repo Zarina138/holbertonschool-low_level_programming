1bc7f19ea3aaf6af210427d919bac36489dc8618
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-05 07:21:21 +0000

    Task

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,25 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * print_chessboard - Prints a standard 8x8 chessboard pattern
+ * print_chessboard - prints the chessboard
+ * @a: 2D array with 8 rows and 8 columns
  */
-void print_chessboard(void)
+void print_chessboard(char (*a)[8])
 {
 	int i, j;
-	
+
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
-			if ((i + j) % 2 == 0)
-			{
-				printf("# ");
-			}
-			else
-			{
-				printf("  ");
-			}
+			_putchar(a[i][j]);
 		}
-		printf("\n");
+		_putchar('\n');
 	}
 }
