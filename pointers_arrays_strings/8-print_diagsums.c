796a51e4bb7019a2ffdae522632c793180aa554e
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-07 21:18:58 +0000

    Task

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -2,21 +2,19 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
- *
-*/
+ * print_diagsums - diagnollarin cemini tapir
+ * @a: massiv
+ * @size: olcusu
+ */
 void print_diagsums(int *a, int size)
 {
-	int i = 0;
-	int cem_esas = 0;
-	int cem_ikinci = 0;
+    int cem_esas = 0;
+    int cem_ikinci = 0;
 
-	for(int i = 0; i < size; i++)
-	{
-		cem_esas += a[i * size + i];
-	        cem_ikinci += a[i * size + (size - 1 - i)];
-	}
-	printf("%d",cem_esas + cem_ikinci);
+    for (int i = 0; i < size; i++)
+    {
+        cem_esas += a[i * size + i];
+        cem_ikinci += a[i * size + (size - 1 - i)];
+    }
+    printf("%d\n", cem_esas + cem_ikinci);
 }
