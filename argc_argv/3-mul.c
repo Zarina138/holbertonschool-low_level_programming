10d627d4d9acbc386a5862ff215ec67984facda6
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-09 18:01:08 +0000

    Task

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * main - Entry point
  * @argc: Argument count
  * @argv: Argument vector
- * Return: Return (Success)
+ * Return: 0 on success, 1 on error
  */
-
 int main(int argc, char *argv[]) 
 {
-	while (argc != 3)
+	if (argc != 3)
+	{
 		printf("Error\n");
-	return (1);
-	
+		return (1);
+	}
 	int eded1 = atoi(argv[1]);
-	int eded2 = atoi(argv[2]);
+        int eded2 = atoi(argv[2]);
 	int cvb = eded1 * eded2;
 	
 	printf("%d\n", cvb);
