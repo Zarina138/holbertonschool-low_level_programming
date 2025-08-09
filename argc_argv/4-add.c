e7d836b5a7ce0ca04608b523a9363b21938b9ade
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-09 18:49:24 +0000

    Task

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,26 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "main.h"
-
 /**
- * main - Entry point
- * @argc: Argument count
- * @argv: Argument vector
- * Return: Return (Success)
+ * main - adds positive numbers.
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
-	int i, n;
-	int cem = 0;
+	int i, j, sum = 0;
 
-	for(i = 1; i < argc; i++)
+	if (argc == 1)
 	{
-		n = atoi(argv[i]);
-
-		if (n > 0)
-			cem += n;
+		printf("0\n");
+		return (0);
 	}
 
-	printf("%d\n", cem);
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
 	return (0);
 }
