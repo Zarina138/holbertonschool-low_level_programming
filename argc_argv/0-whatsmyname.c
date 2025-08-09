447ed289a8dc60ab2851fbd591aef3176aea8234
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-09 15:26:30 +0000

    Task

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,10 +3,10 @@
 /*
  * main - Entry point
  * @argc: Argument count
- * @argv: Argument Vector 
+ * @argv: Argument Vector
  */
-int main(int argc __attribute__((unused)),char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
