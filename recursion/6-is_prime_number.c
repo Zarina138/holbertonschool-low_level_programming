fee6cbf1b33d77f792ff0bd006f03add629da6d1
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-09 12:49:48 +0000

    Task

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -8,11 +8,11 @@
  */
 int _is_prime_helper(int n, int divisor)
 {
-        if (divisor * divisor > n)
-                return (1);
-        if (n % divisor == 0)
-                return (0);
-        return (_is_prime_helper(n, divisor + 1));
+	if (divisor * divisor > n)
+		return (1);
+	if (n % divisor == 0)
+		return (0);
+	return (_is_prime_helper(n, divisor + 1));
 }
 /**
  * is_prime_number - returns 1 if n is prime, otherwise 0
@@ -22,7 +22,7 @@ int _is_prime_helper(int n, int divisor)
  */
 int is_prime_number(int n)
 {
-        if (n <= 1)
-                return (0);
-        return (_is_prime_helper(n, 2));
+	if (n <= 1)
+		return (0);
+	return (_is_prime_helper(n, 2));
 }
