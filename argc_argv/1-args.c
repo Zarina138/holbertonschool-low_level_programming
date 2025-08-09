ea2b7476d950466ee1de75a6b2a27fbc2e1ee930
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-09 15:56:01 +0000

    Task

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -7,7 +7,7 @@
  *
  * Return: return
  */
-int main(int argc, char *arv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
