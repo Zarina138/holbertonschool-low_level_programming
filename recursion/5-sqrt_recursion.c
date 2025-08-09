5f8ee7834d57748a87806097ef448796b7ae5d71
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-09 12:24:39 +0000

    Task

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *  _sqrt_recursion - the natural square root of a number
+ *  _sqrt_helper: Komekci funksiya
+ *  @i: i ,kokalti eded deye bilerik bir nov
+ *  @n: n
+ *  Return: Return right answer
+ */
+int _sqrt_helper(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	else if (i * i > n)
+		return (-1);
+	return _sqrt_helper(n, i + 1);
+}
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return _sqrt_helper(n, 0);
+}
