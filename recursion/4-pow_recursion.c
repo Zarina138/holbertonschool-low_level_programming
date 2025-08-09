b60c32e5708f7f29533578f362efab56aa5fa15d
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-09 10:12:27 +0000

    Task

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 /**
- * _pow_recursion - function that returns the value of x raised to the power of y
+ * _pow_recursion - returns the value of x raised to the power of y
  * @x: num
  * @y: num
  * Return: returns the value of x raised to the power of y
