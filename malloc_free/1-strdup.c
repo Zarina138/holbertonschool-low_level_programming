d682fe39cd02e8a4d26e85be38d2a00f0d9d9895
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-13 16:05:30 +0000

    Task

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+unsigned int my_length(char *s)
+{
+	unsigned int len;
+	unsigned int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	return len;
+}
+char *_strdup(char *str)
+{
+	char *cpy;
+	unsigned int i;
+	unsigned int len = my_strlen(str) + 1;
+
+	cpy = malloc(len);
+	if (!cpy)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		cpy[i] = str[i];
+	return (cpy);
+}
