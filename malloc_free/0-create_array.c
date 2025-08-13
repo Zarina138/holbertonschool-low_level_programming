be65a472e0529235ae78f68e4ce05fdb205ffc78
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-13 15:02:15 +0000

    Task

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *
+ *
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	if (size == 0)
+		return NULL;
+	char *array = malloc(size * sizeof(char));
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		array[i] = c;
+	return array;
+}
