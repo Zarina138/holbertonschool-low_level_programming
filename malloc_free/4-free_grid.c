c97a09e74ec19f1c2b1b6ec4b2789966fc6fce09
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-16 08:53:29 +0000

    Task

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * free_grid - free up a 2d grid
+ * @grid: double pointer 2d grid
+ * @height: height of grid
+ */
 void free_grid(int **grid, int height)
 {
 	int a;
