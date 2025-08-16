03ef652bd3dac3d9d4faecd8b2cf25dfd2507a00
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-16 19:28:51 +0000

    Task

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -4,14 +4,15 @@
 /**
  * malloc_checked - a function that allocates memory using
  * @b: memory size
- *
+ * Return: pointer
  */
 void *malloc_checked(unsigned int b)
 {
 	void *z;
 
 	z = malloc(b);
+
 	if (z == NULL)
-		exit (98);
+		exit(98);
 	return (z);
 }
