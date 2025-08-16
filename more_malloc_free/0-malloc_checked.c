4bcceb4df101d10a752e2de170cebc273792feea
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-16 13:37:48 +0000

    Task

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - a function that allocates memory using
+ * @b: memory size
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *z;
+
+	z = malloc(b);
+	if (z == NULL)
+		exit (98);
+}
