4daf7324d758b7ce0df66eb73e79955e424bec63
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-22 05:54:13 +0000

    Task

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,23 +1,6 @@
 #include "3-calc.h"
 #include <stddef.h>
-/**
- * _strcmp - compares two strings
- * @s1: first string
- * @s2: second string
- *
- * Return: 0 if equal, non-zero otherwise
- */
-int _strcmp(char *s1, char *s2)
-{
-    while (*s1 && *s2)
-    {
-        if (*s1 != *s2)
-            return (*s1 - *s2);
-        s1++;
-        s2++;
-    }
-    return (*s1 - *s2);
-}
+#include <string.h>
 
 /**
  * get_op_func - selects the correct function to perform operation
@@ -27,21 +10,21 @@ int _strcmp(char *s1, char *s2)
  */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-    while (ops[i].op != NULL)
-    {
-        if (_strcmp(ops[i].op, s) == 0)
-            return (ops[i].f);
-        i++;
-    }
-    return (NULL);
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
