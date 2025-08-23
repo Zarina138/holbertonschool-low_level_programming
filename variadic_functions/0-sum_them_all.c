b8b5fbe72c2384cb09bc4c25836ba53d6d0d99b3
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-23 11:20:55 +0000

    Task

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -4,16 +4,17 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
+	int x;
 	int sum;
 	va_list task0;
-	va_arg(task0,int);
+	x = va_arg(task0,int);
 
 	if (n == 0)
 		return(0);
 	va_start(task0,n);
 
 	for (i=0; i<=n;i++)
-		sum+=i;
+		sum+=x;
 	va_end(task0);
 	return(sum);
 }
