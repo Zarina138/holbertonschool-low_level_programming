de60d5c590cd175e54b62c1ff2dd7539489bef3e
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-23 12:46:40 +0000

    Task

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -2,14 +2,15 @@
 #include <stdarg.h>
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+    unsigned int i;	
     va_list task1;
     va_start(task1,n);
 
     if (separator == NULL)
         return;
 
-    for (int i = 0; i < n; i++)
-        printf("%s","%d",separator,va_arg(task1,int));
+    for (i = 0; i < n; i++)
+        printf("%s, %d",separator,va_arg(task1,int));
     printf("\n");
     va_end(task1);
 
