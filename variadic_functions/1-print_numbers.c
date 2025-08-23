db424ae65a2cdc626d91cc864ad5f880a2187511
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-23 13:41:11 +0000

    Task

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -6,25 +6,29 @@
  * @separator: string to print between numbers
  * @n: number of integers to print
  *
+ * Description: This function prints all integer arguments passed
+ * after the 'n' parameter, separated by the string 'separator'.
+ * If 'separator' is NULL, no separator is printed. The separator
+ * is printed only between numbers, not before the first one.
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    unsigned int i;
-    va_list task1;
+	va_list task1;
+	unsigned int i;
 
-    va_start(task1, n);
+	va_start(task1, n);
 
-    for (i = 0; i < n; i++)
-    {
-        if (separator != NULL && i > 0)
-        {
-            printf("%s", separator);
-            printf("%d", va_arg(task1, int));
-        }
-	else
-            printf("%d", va_arg(task1,int));
-    }
+	for (i = 0; i < n; i++)
+	{
+		if (separator != NULL && i > 0)
+		{
+			printf("%s", separator);
+			printf("%d", va_arg(task1, int));
+		}
+		else
+			printf("%d", va_arg(task1, int));
+	}
 
-    printf("\n");
-    va_end(task1);
+	printf("\n");
+	va_end(task1);
 }
