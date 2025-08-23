0ca596d912b38abe024ef7cd0dfe56b4112fe838
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-23 18:24:38 +0000

    Task

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ */
 void print_all(const char * const format, ...)
 {
 	int i = 0;
