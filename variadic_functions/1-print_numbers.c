9f3aa08710e50e0c1aff36b627f2d9f295cad0e1
Zarina138 <zrinaabdulazizova@gmail.com>
2025-08-23 13:18:00 +0000

    Task

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -6,18 +6,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
     va_list task1;
     va_start(task1,n);
 
-    if (separator == NULL)
-        return;
-
     for (i = 0; i < n; i++)
-        printf("%s, %d",separator,va_arg(task1,int));
+    {
+	    if (seperator != NULL && i > 0)
+	    {
+		    printf("%s", seperator);
+		    printf("%d",va_arg(task1,int));
+	    }
+	    else
+		    printf("%d", va_arg(task1,int));
+    }
     printf("\n");
     va_end(task1);
-
-
-}
-int main(void)
-{
-    print_numbers(", ", 4, 0, 98, -1024, 402);
-    return (0);
 }
