17ae1a481b8c19d9b0ee65562b20d0d8df6a12f3
Zarina138 <zrinaabdulazizova@gmail.com>
2025-09-10 21:38:20 +0000

    Task

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -13,7 +13,7 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *temp;
 	unsigned int len = 0;
-	
+
 	if (head == NULL || str == NULL)
 		return (NULL);
 	new_node = malloc(sizeof(list_t));
@@ -29,8 +29,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		len++;
 	new_node->len = len;
 	new_node->next = NULL;
-	
-	if (*head == NULL)
+		if (*head == NULL)
 	{
 		*head = new_node;
 	}
