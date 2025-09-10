ddb6bf7319595c397af7d3a3c2f07a6efc90caac
Zarina138 <zrinaabdulazizova@gmail.com>
2025-09-10 08:21:28 +0000

    Task

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -4,6 +4,14 @@
 
 #include <stddef.h>
 
+/**
+ * struct elements - singly linked list node
+ * @str: string (malloc'ed string)
+ * @len: length of the string
+ * @next: pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
 int _putchar(char c);
 typedef struct elements
 {
