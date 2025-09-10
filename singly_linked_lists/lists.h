22ccb3761249a4df57a3c5986b3c51cf59b2c8fd
Zarina138 <zrinaabdulazizova@gmail.com>
2025-09-10 08:05:07 +0000

    Task

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,12 +1,15 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+
+#include <stddef.h>
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 typedef struct elements
 {
 	char *str;
 	int len;
-	struct element* next;
+	struct elements* next;
 }list_t;
 #endif
