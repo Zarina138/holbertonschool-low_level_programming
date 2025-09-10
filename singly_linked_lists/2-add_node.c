b0b08fa83dcc23d9fb4f9e51b48d09101421830d
Zarina138 <zrinaabdulazizova@gmail.com>
2025-09-10 21:41:20 +0000

    Task

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,28 +11,29 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
-    unsigned int len = 0;
+	list_t *new_node;
+	unsigned int len = 0;
 
-    if (head == NULL || str == NULL)
-        return (NULL);
+	if (head == NULL || str == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
-    while (str[len] != '\0')
-        len++;
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node->len = len;
-    new_node->next = *head;
-    *head = new_node;
+	while (str[len] != '\0')
+		len++;
 
-    return (new_node);
+	new_node->len = len;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
