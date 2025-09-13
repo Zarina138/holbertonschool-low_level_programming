e9c1c4af21913112f3f52eb9f6b613dd969bee67
Zarina138 <zrinaabdulazizova@gmail.com>
2025-09-13 15:44:12 +0000

    Task

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 	
-	for (i = 0; idx != 1; i++)
+	for (i = 0; i < idx - 1; i++)
 	{
 		if (current == NULL)
 			return (NULL);
