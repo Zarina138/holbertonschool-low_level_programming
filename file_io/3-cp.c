591b14f2788a8103664e7ea6ab01029d104e8c16
Zarina138 <zrinaabdulazizova@gmail.com>
2025-09-13 19:37:59 +0000

    Task

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,66 +1,103 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 
-#define BUFFER_SIZE 1024
+char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
- * print_error_and_exit - Prints an error message to STDERR and exits.
- * @exit_code: The exit code.
- * @message: The error message (with %s if filename should be printed).
- * @filename: The file name related to the error (can be NULL).
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
+ *
+ * Return: A pointer to the newly-allocated buffer.
  */
-void print_error_and_exit(int exit_code, const char *message, const char *filename)
+char *create_buffer(char *file)
 {
-	if (filename)
-		dprintf(STDERR_FILENO, message, filename);
-	else
-		dprintf(STDERR_FILENO, "%s", message);
-	exit(exit_code);
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
 }
 
 /**
- * main - Copies the content of one file to another.
- * @argc: Number of arguments.
- * @argv: Array of arguments.
- *
- * Return: 0 on success, otherwise exits with error code.
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
  */
-int main(int argc, char **argv)
+void close_file(int fd)
 {
-	int input_fd, output_fd, read_count, write_count;
-	char buffer[BUFFER_SIZE];
+	int c;
+
+	c = close(fd);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ *              If file_from does not exist or cannot be read - exit code 98.
+ *              If file_to cannot be created or written to - exit code 99.
+ *              If file_to or file_from cannot be closed - exit code 100.
+ */
+int main(int argc, char *argv[])
+{
+	int from, to, r, w;
+	char *buffer;
 
 	if (argc != 3)
-		print_error_and_exit(97, "Usage: cp file_from file_to\n", NULL);
-
-	input_fd = open(argv[1], O_RDONLY);
-	if (input_fd == -1)
-		print_error_and_exit(98, "Error: Can't read from file %s\n", argv[1]);
-
-	output_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (output_fd == -1)
-		print_error_and_exit(99, "Error: Can't write to %s\n", argv[2]);
-
-	while (1)
 	{
-		read_count = read(input_fd, buffer, BUFFER_SIZE);
-		if (read_count == -1)
-			print_error_and_exit(98, "Error: Can't read from file %s\n", argv[1]);
-		if (read_count == 0) /* EOF */
-			break;
-
-		write_count = write(output_fd, buffer, read_count);
-		if (write_count == -1 || write_count != read_count)
-			print_error_and_exit(98, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 
-	if (close(input_fd) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", input_fd), exit(100);
-	if (close(output_fd) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", output_fd), exit(100);
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
+	free(buffer);
+	close_file(from);
+	close_file(to);
 
 	return (0);
 }
