d48837f41b77107a67fb8da07fbd3f8555514fce
Zarina138 <zrinaabdulazizova@gmail.com>
2025-09-13 15:20:05 +0000

    Task

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -9,14 +9,13 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-   unsigned  int i;
-
-    for (i = 0; i < index; i++)
-    {
-        if (head == NULL)
-            return (NULL);
-
-        head = head->next;
-    }
-    return (head);
+	unsigned  int i;
+	
+	for (i = 0; i < index; i++)
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+	}
+	return (head);
 }
